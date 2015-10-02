c9ae6d6484608369967bd8188b9c09b447a9f86c
ChoiMun gyu <cinari4@gmail.com>
2015-10-02 23:44:14 +0900

    - init new project - simple app converting lbs to kg

---
+++ UnitConverter/UnitConverter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  UnitConverter
+//
+//  Created by ChoiMun gyu on 2015. 10. 2..
+//  Copyright © 2015년 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var kgText: UITextField!
+    
+    @IBAction func convertKg(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
