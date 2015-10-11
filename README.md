5328fc14440e38b04f5f79bfaffc2dfb74b9071f
ChoiMun gyu <cinari4@gmail.com>
2015-10-12 00:44:04 +0900

    - change directory - add tabor application( based single view application )

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # dailycode
 - FoodTracker ( Start Developing iOS Apps with Swift )
 https://developer.apple.com/library/prerelease/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/index.html#//apple_ref/doc/uid/TP40015214-CH2-SW1
+
+- XcodePractice
+practice iOS 8 App Development Essentials
+
+- WeightTracker
+A prototype website has kg<->lbs converter, workout diary
---
+++ XcodePractice/TabBar/TabBar/Tab2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Tab2ViewController.swift
+//  TabBar
+//
+//  Created by ChoiMun gyu on 2015. 10. 12..
+//  Copyright © 2015년 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class Tab2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
