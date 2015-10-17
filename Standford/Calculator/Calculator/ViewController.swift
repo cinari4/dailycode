e3dad23ae45f4710204d74df3aa83049a96a7681
ChoiMun gyu <cinari4@gmail.com>
2015-10-17 22:20:41 +0900

    - lesson 2 - implements multiple number

--- Standford/Calculator/Calculator/ViewController.swift
+++ Standford/Calculator/Calculator/ViewController.swift
@@ -11,20 +11,55 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
-    
-    var userType:Bool = false
+ 
+    var isUserType:Bool = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        if userType {
+        if isUserType {
             display.text = display.text! + digit
         } else {
             display.text = digit
-            userType = true
+            isUserType = true
             
         }
        
     }
     
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isUserType {
+            enter()
+        }
+        switch operation {
+        case "×":
+            if operandStack.count >= 2 {
+                displayValue = operandStack.removeLast() * operandStack.removeLast()
+                enter()
+            }
+//        case "÷":
+//        case "+":
+//        case "−":
+        default: break
+        }
+    }
+    
+    
+    var operandStack: Array<Double> = Array<Double>()
+    @IBAction func enter() {
+        isUserType = false
+        operandStack.append(displayValue)
+        print("\(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            isUserType = false
+        }
+    }
 }
 
