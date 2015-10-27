8cb44aa8e40f872733415fa958ed1c529e2b8191
ChoiMun gyu <cinari4@gmail.com>
2015-10-27 23:02:13 +0900

    - add button to open photo library

--- RememberTheDay/RememberTheDay/FirstViewController.swift
+++ RememberTheDay/RememberTheDay/FirstViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBAction func testBtn(sender: AnyObject) {
+        let imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.delegate = self
+        self.presentViewController(imagePickerViewController,
+            animated: true,
+            completion: nil)
+    }
 
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let imageName = "not-available.jpg"
         let image = UIImage(named: imageName);
         let imageView = UIImageView(image: image!);
@@ -21,8 +30,16 @@ class FirstViewController: UIViewController {
         let screenWidth = bounds.size.width
         let screenHeight = bounds.size.height
         
-        imageView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        imageView.frame = CGRect(x: 0, y: 0, width: screenWidth/2, height: screenHeight/2)
         view.addSubview(imageView)
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        
 
     }
 
