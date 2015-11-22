9587cfad800cdbad7e6918653b4084dc8401ce26
ChoiMun gyu <cinari4@gmail.com>
2015-11-23 02:18:27 +0900

    test multi pins on google map

--- RememberTheDay/RememberTheDay/SecondViewController.swift
+++ RememberTheDay/RememberTheDay/SecondViewController.swift
@@ -12,11 +12,16 @@ import GoogleMaps;
 
 class SecondViewController: UIViewController {
     
+    var photoInfoList:[PhotoInfo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstViewController = storyboard!.instantiateViewControllerWithIdentifier("firstViewController") as! FirstViewController
+        photoInfoList = firstViewController.photoInfoList
+
         let camera = GMSCameraPosition.cameraWithLatitude(-33.868,
-            longitude:151.2086, zoom:6)
+            longitude:151.2086, zoom:2)
         let mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
         
         let marker = GMSMarker()
@@ -24,6 +29,13 @@ class SecondViewController: UIViewController {
         marker.snippet = "Hello World"
         marker.appearAnimation = kGMSMarkerAnimationPop
         marker.map = mapView
+        
+        
+        let marker2 = GMSMarker()
+        marker2.position = CLLocationCoordinate2DMake(+65.68289500, -17.54892833)
+        marker2.snippet = "Hello World"
+        marker2.appearAnimation = kGMSMarkerAnimationPop
+        marker2.map = mapView
         
         self.view = mapView
 
