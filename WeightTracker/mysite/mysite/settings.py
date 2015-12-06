e0de943c9ef7d07d9a34a17441cba6e289984b5c
ChoiMun gyu <cinari4@gmail.com>
2015-12-07 02:07:08 +0900

    add Classified View replace TEMPLATE_DIRS to TEMPLATE

--- WeightTracker/mysite/mysite/settings.py
+++ WeightTracker/mysite/mysite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,5 +103,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
