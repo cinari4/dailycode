c507551c0718c291a444ffe708baa3fcf9e65b7f
ChoiMun gyu <cinari4@gmail.com>
2015-12-17 21:49:10 +0900

    divide pages

--- WeightTracker/python-getting-started-master/gettingstarted/urls.py
+++ WeightTracker/python-getting-started-master/gettingstarted/urls.py
@@ -9,9 +9,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    # url(r'^db', hello.views.db, name='db'),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', weighttracker.views.index, name='index'),
-    #url(r'^db', hello.views.db, name='db'),
-    #url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^weighttracker/convert', weighttracker.views.convert, name='convert'),
+    url(r'^weighttracker/divide', weighttracker.views.divide, name='divide'),
+    url(r'^weighttracker/dailypost', weighttracker.views.dailypost, name='dailypost'),
 )
--- WeightTracker/python-getting-started-master/weighttracker/views.py
+++ WeightTracker/python-getting-started-master/weighttracker/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def convert(request):
+    return render(request, 'convert.html')
+
+def divide(request):
+    return render(request, 'divide.html')
+
+def dailypost(request):
+    return render(request, 'dailypost.html')
