2d16fec6127935f3c8ce1a6ac5246490e688910a
ChoiMun gyu <cinari4@gmail.com>
2015-12-29 23:15:37 +0900

    - add divide page - add requests in requirements.txt

--- WeightTracker/python-getting-started-master/weighttracker/views.py
+++ WeightTracker/python-getting-started-master/weighttracker/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+import requests
 
 def index(request):
-    return render(request, 'index.html')
+	r = requests.get('http://httpbin.org/status/418')
+	return HttpResponse('<pre>' + r.text + '</pre>')
 
 def convert(request):
     return render(request, 'convert.html')
