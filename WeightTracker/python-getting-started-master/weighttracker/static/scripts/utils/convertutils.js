934761573cace4b1d24ef56935d75ad8b722baec
ChoiMun gyu <cinari4@gmail.com>
2016-01-04 22:54:55 +0900

    add divide-page functions

--- WeightTracker/python-getting-started-master/weighttracker/static/scripts/utils/convertutils.js
+++ WeightTracker/python-getting-started-master/weighttracker/static/scripts/utils/convertutils.js
@@ -4,6 +4,7 @@
 const POUNDS_TO_KILOGRAMS = 0.453592;
 const KILOGRAMS_TO_POUNDS = 2.20462;
 const DEFAULT_BARBEL_KG = 20;
+const DEFAULT_BARBEL_LBS = 44.0925;
 
 function lbsToKg(lbs) {
     if(checkValidation(lbs))
@@ -26,6 +27,10 @@ function kgToBarbellPlate(kg) {
     var kgRemain = kg - DEFAULT_BARBEL_KG;
     var returnStr = "";
 
+    if(kg <= DEFAULT_BARBEL_KG) {
+        return "shuld be over " + DEFAULT_BARBEL_KG + "kg";
+    }
+
     for(index=0; index<kgList.length; index++) {
         var divisor = parseInt(kgRemain/kgList[index]);
         if(divisor > 0){  
@@ -37,7 +42,39 @@ function kgToBarbellPlate(kg) {
     for(index=0; index<kgList.length; index++) {
         returnStr += (kgList[index] + "kg x " + kgCount[index] + "\n");
     }
-    returnStr += "remain : " + kgRemain;
+
+    returnStr += "bar is : " + DEFAULT_BARBEL_KG + "kg" + "\n";
+    returnStr += "remain : " + kgRemain + "kg";
+    return returnStr;
+}
+
+function lbsToBarbellPlate(lbs) {
+    if(checkValidation(lbs))
+        return false;
+
+    var lbsList = [45, 35, 25, 15, 10, 5];
+    var lbsCount = [0, 0, 0, 0, 0, 0];
+    var lbsRemain = lbs - DEFAULT_BARBEL_LBS;
+    var returnStr = "";
+
+    if(lbs <= DEFAULT_BARBEL_LBS) {
+        return "shuld be over " + DEFAULT_BARBEL_LBS + "lbs";
+    }
+
+    for(index=0; index<lbsList.length; index++) {
+        var divisor = parseInt(lbsRemain/lbsList[index]);
+        if(divisor > 0){  
+            lbsCount[index] = divisor;
+            lbsRemain = lbsRemain % lbsList[index];
+        }
+    }
+    
+    for(index=0; index<lbsList.length; index++) {
+        returnStr += (lbsList[index] + "lbs x " + lbsCount[index] + "\n");
+    }
+
+    returnStr += "bar is : " + DEFAULT_BARBEL_LBS + "lbs" + "\n";
+    returnStr += "remain : " + lbsRemain + "lbs";
     return returnStr;
 }
 
