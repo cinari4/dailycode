9d185fdf8a6c7023b396cf446241c6089930151c
ChoiMun gyu <cinari4@gmail.com>
2016-01-11 23:25:06 +0900

    add hackerrank_dailycode9

---
+++ hackerrank_dailycode/day9/code.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int gcd=find_gcd(a,b);
+        System.out.println(gcd);
+    }
+   static  int find_gcd(int a,int b){
+	   if(a == b) {
+		   return a;
+	   } else {
+		   return find_gcd(Math.max(a, b)-Math.min(a, b), Math.min(a, b));
+	   }
+   }
+}
