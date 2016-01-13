941b53ee094b41146f480bdf0dfb89c737f5999b
ChoiMun gyu <cinari4@gmail.com>
2016-01-14 00:00:01 +0900

    fix boundary bug on hackerrank_dailycode11

--- hackerrank_dailycode/day11/code.java
+++ hackerrank_dailycode/day11/code.java
@@ -5,12 +5,12 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-	
-	// 1*7
-	public static final int MIN_SUM_HOURGLASS = 7;
-	// 9*7
-	public static final int MAX_SUM_HOURGLASS = 63;
-	
+    
+    // -9*7
+    public static final int MIN_SUM_HOURGLASS = -63;
+    // 9*7
+    public static final int MAX_SUM_HOURGLASS = 63;
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
@@ -21,30 +21,23 @@ public class Solution {
             }
         }
         
-        int max=0;
+        int max=MIN_SUM_HOURGLASS;
         int temp=0;
         
         for(int i=1; i<=4; i++) {
-        	for(int j=1; j<=4; j++) {
-        		if(arr[i][j] == 0)
-        			continue;
-        		
-        		// sum hourglass form
-        		temp = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] +arr[i+1][j+1];
-        		
-        		// not hourglass form
-        		if(temp < MIN_SUM_HOURGLASS)
-        			continue;
-        		
-        		// find max sum
-        		if(temp == MAX_SUM_HOURGLASS) {
-        			max = MAX_SUM_HOURGLASS;
-        			break;
-        		}
-        		
-        		if(temp > max)
-        			max = temp;
-        	}
+            for(int j=1; j<=4; j++) {
+                // sum hourglass form
+                temp = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] +arr[i+1][j+1];
+                
+                // find max sum
+                if(temp == MAX_SUM_HOURGLASS) {
+                    max = MAX_SUM_HOURGLASS;
+                    break;
+                }
+                
+                if(temp > max)
+                    max = temp;
+            }
         }
         
         System.out.println(max);
