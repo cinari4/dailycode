071e1472eceb33d2abd56e8cfd872ceba2f824e5
ChoiMun gyu <cinari4@gmail.com>
2016-02-01 22:53:39 +0900

    fix code, cuz timeout

--- hackerrank_dailycode/project-euler/001/code.java
+++ hackerrank_dailycode/project-euler/001/code.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Solution {
@@ -7,27 +8,16 @@ public class Solution {
         Scanner sc=new Scanner(System.in);
         int T=sc.nextInt();
         
-        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<T; i++) {
+            int temp = sc.nextInt();
+            long three = (temp-1)/3;
+            long five = (temp-1)/5;
+            long fifteen = (temp-1)/15;
+            long total = 3*(three*(three+1)/2) + 5*(five*(five+1)/2) - 15*(fifteen*(fifteen+1)/2);
+            BigInteger sum = new BigInteger(String.valueOf(total));
+            
+            System.out.println(sum);
+        }
         
-    	for(int i=0; i<T; i++) {
-    		int temp = sc.nextInt();
-    		int sum = 0;
-    		
-    		for(int j=3; j<temp; j+=3) {
-    			sum +=j;
-    		}
-    		
-    		for(int j=5; j<temp; j+=5) {
-    			sum +=j;
-    		}
-    		
-    		for(int j=15; j<temp; j+=15) {
-    			sum -=j;
-    		}
-    		
-    		sb.append(sum+"\n");
-    	}
-    	
-    	System.out.println(sb.toString());
     }
 }
