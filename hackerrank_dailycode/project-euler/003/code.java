252a54b7f27d58653ce0d3daa2fff44ade84d0f1
ChoiMun gyu <cinari4@gmail.com>
2016-02-03 22:38:38 +0900

    add project-euler003

---
+++ hackerrank_dailycode/project-euler/003/code.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    	Scanner sc = new Scanner(System.in);
+    	int max = sc.nextInt();
+    	while(max-->0) {
+    		System.out.println(getMaxPrime(sc.nextLong()));	
+    	}
+    }
+    
+    public static long getMaxPrime(long max) {
+    	for(long i=max; i>2; i--) {
+    		if(max%i==0 && i%2!=0) {
+    			if(checkPrime(i))
+    				return i;
+    		}
+    	}
+    	return 2;
+    }
+
+	private static boolean checkPrime(long n) {
+	    if(n < 2) return false;
+	    if(n == 2 || n == 3) return true;
+	    if(n%2 == 0 || n%3 == 0) return false;
+	    long sqrtN = (long)Math.sqrt(n)+1;
+	    for(long i = 6L; i <= sqrtN; i += 6) {
+	        if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+	    }
+	    return true;	
+	}
+}
