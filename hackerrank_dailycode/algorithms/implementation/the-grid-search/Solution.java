9da4168113d8bd0f12732e78b2f4da88fa026bcd
ChoiMun gyu <cinari4@gmail.com>
2016-03-01 21:22:58 +0900

    fix bugs, remove unused codes

--- hackerrank_dailycode/algorithms/implementation/the-grid-search/Solution.java
+++ hackerrank_dailycode/algorithms/implementation/the-grid-search/Solution.java
@@ -2,70 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	public static class Grid {
-		public int x;
-		public int y;
-		public String[] arr;
-		
-		public Grid(int x, int y, String[] arr) {
-			this.x = x;
-			this.y = y;
-			this.arr = arr;
-		}
-	}
-	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cntTestCase = sc.nextInt();
         
         for(int i=0; i<cntTestCase; i++) {
-        	int y1 = sc.nextInt();
-        	int x1 = sc.nextInt();
-        	String[] arr1 = new String[y1];
-        	for(int j=0; j<y1; j++) {
-        		arr1[j] = sc.next();
-        	}
-        	
-        	int y2 = sc.nextInt();
-        	int x2 = sc.nextInt();
-        	String[] arr2 = new String[y2];
-        	for(int j=0; j<y2; j++) {
-        		arr2[j] = sc.next();
-        	}
-        	
-        	System.out.println(findGrid(x1, y1, arr1, x2, y2, arr2));
+            int y1 = sc.nextInt();
+            int x1 = sc.nextInt();
+            String[] arr1 = new String[y1];
+            for(int j=0; j<y1; j++) {
+                arr1[j] = sc.next();
+            }
+            
+            int y2 = sc.nextInt();
+            int x2 = sc.nextInt();
+            String[] arr2 = new String[y2];
+            for(int j=0; j<y2; j++) {
+                arr2[j] = sc.next();
+            }
+            
+            System.out.println(findGrid(x1, y1, arr1, x2, y2, arr2));
         }
         
     }
     
     public static String findGrid(int x1, int y1, String arr1[], int x2, int y2, String arr2[]) {
-    	boolean find;
-    	int posSubString;
-    	
-    	for(int i=0; i<y1-y2; i++) {
-    		find = true;
-    		posSubString = -1;
-    		inner:
-    		for(int j=0, k=i;j<y2; j++,k++) {
-    			//System.out.println("arr1:"+arr1[k]+"\tarr2:"+arr2[j]);
-    			if(!arr1[k].contains(arr2[j])) {
-    				find = false;
-    				break inner;
-    			} else {
-    				if(posSubString == -1) {
-    					posSubString = arr1[k].indexOf(arr2[j]);
-    				} else if(posSubString != arr1[k].indexOf(arr2[j])) {
-    					find = false;
-    					break inner;
-    				}
-    			}
-    		}
-    		
-    		if(find==true)
-    			return "YES";
-    	}
-    	
-    	return "NO"; 
+        for(int i=0; i<=y1-y2; i++) {
+            int x = arr1[i].indexOf(arr2[0]);
+            if(x==-1)
+                continue;
+            
+            if(matchGrid(arr1, arr2, x, x+x2, i))
+                return "YES";
+            
+            while(x>=0) {
+                x = arr1[i].indexOf(arr2[0], x+1);
+                if(x==-1)
+                    break;
+                if(matchGrid(arr1, arr2, x, x+x2, i))
+                    return "YES";
+            }
+        }
+        
+        return "NO"; 
     }
-	
+    
+    public static boolean matchGrid(String arr1[], String arr2[], int startX, int endX, int startY) {
+        int tmp = startY;
+        for(int i=0; i<arr2.length; i++, tmp++) {
+            if(!arr1[tmp].substring(startX, endX).equals(arr2[i]))
+                return false;
+        }
+        return true;
+    }
+    
 }
