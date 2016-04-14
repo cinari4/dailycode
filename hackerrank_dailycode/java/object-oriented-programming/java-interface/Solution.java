9e5e09446f59fca4cbaa7b17fc500bc08c174bc4
ChoiMun gyu <cinari4@gmail.com>
2016-04-14 23:02:46 +0900

    add hackerrank /java/object-oriented-programming

---
+++ hackerrank_dailycode/java/object-oriented-programming/java-interface/Solution.java
@@ -0,0 +1,65 @@
+import java.util.*;
+interface AdvancedArithmetic{
+  public abstract int divisorSum(int n);
+}
+
+
+class MyCalculator implements AdvancedArithmetic {
+	@Override
+	public int divisorSum(int n) {
+		int sum = 0;
+		Set<Integer> setDivisor = new HashSet<Integer>();
+		for (int i = 1; i<=n; i++) {
+			if (n % i == 0) {
+				setDivisor.add(i);
+			}
+		}
+		
+		for(int temp : setDivisor) {
+			sum += temp;
+		}
+		return sum;
+	}
+}
+
+
+class Solution{
+
+    public static void main(String []argh)
+    {
+        MyCalculator my_calculator=new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print(my_calculator.divisorSum(n)+"\n");
+
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o)
+    {
+
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++)
+        {
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+
+/* input 
+
+6
+
+*/
+
+/* output
+
+I implemented: AdvancedArithmetic
+12
+
+*/
+
