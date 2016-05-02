f15f27bcc77b8d0406d5087c7547af97c751ceb8
ChoiMun gyu <cinari4@gmail.com>
2016-05-02 22:43:59 +0900

    add hackerrank /algorithms/implementation/encryption

--- hackerrank_dailycode/algorithms/implementation/encryption/Solution.java
+++ hackerrank_dailycode/algorithms/implementation/encryption/Solution.java
@@ -1,40 +1,41 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String origStr = in.next();
-       
-        origStr = origStr.replaceAll("\\s","");
-        
-        double sqrt = Math.sqrt(origStr.length());
-        int width = (int)Math.floor(sqrt);
-        int height = (int)Math.ceil(sqrt);
-       
-        if (!(width * height >= origStr.length())) {
-        	width++;
-        }
-        
-        char[][] arrEnc = new char[width][height];
-        int pos = 0;
-        for (int i = 0; i < width; i++) {
-        	for (int j = 0; j < height; j++) {
-        		if (pos >= origStr.length()) {
-        			break;
-        		}
-        		
-        		arrEnc[i][j] = origStr.charAt(pos++);
-        	}
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < height; i++) {
-        	for (int j = 0; j< width; j++) {
-        		sb.append(arrEnc[j][i]);
-        	}
-        	sb.append(" ");
-        }
-        System.out.println(sb.toString());
-    }
-        
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String plainText = in.next();
+		int lenPlainText = plainText.length();
+		
+		int row, coloum;
+		row = (int) Math.floor(Math.sqrt(lenPlainText));
+		coloum = (int) Math.ceil(Math.sqrt(lenPlainText));
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < coloum; i++) {
+			int j = i;
+			while (j < lenPlainText) {
+				sb.append(plainText.charAt(j));
+				j+=coloum;
+			}
+			sb.append(" ");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
 }
+
+/* input
+
+haveaniceday
+
+*/
+
+/* output
+
+hae and via ecy
+
+*/
