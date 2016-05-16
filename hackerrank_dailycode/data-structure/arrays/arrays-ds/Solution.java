a77eb5a8d56d2af8dc5ef15a41dfc4f55e9e8913
ChoiMun gyu <cinari4@gmail.com>
2016-05-16 21:49:10 +0900

    add hackerrank /data-structure/arrays/arrays-ds

---
+++ hackerrank_dailycode/data-structure/arrays/arrays-ds/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+			int n = in.nextInt();
+			int arr[] = new int[n];
+			StringBuilder sb = new StringBuilder();
+			
+			for(int i = 0; i < n ; i++) {
+				arr[i] = in.nextInt();
+			}
+			
+			for(int i = n-1; i >=0; i--) {
+				sb.append(arr[i]).append(" ");
+			}
+			
+			System.out.println(sb.toString());
+	}
+}
