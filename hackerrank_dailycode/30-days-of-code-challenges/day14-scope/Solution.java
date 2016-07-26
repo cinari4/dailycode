ab975769289377270466f63fff672e00f639c8dd
ChoiMun gyu <cinari4@gmail.com>
2016-07-27 00:58:39 +0900

    day14-scope

---
+++ hackerrank_dailycode/30-days-of-code-challenges/day14-scope/Solution.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Difference {
+	private int[] elements;
+	public int maximumDifference;
+	
+	public Difference(int[] a) {
+		this.elements = a;
+	}
+	
+	public void computeDifference() {
+		maximumDifference = Integer.MIN_VALUE;
+		for (int i = 0; i < elements.length-1; i++) {
+			for (int j = 0; j < elements.length; j++) {
+				maximumDifference = Math.max(maximumDifference, Math.abs(elements[i] - elements[j]));
+			}
+		}
+	}
+	
+} // End of Difference class
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		sc.close();
+
+		Difference difference = new Difference(a);
+		difference.computeDifference();
+
+	System.out.print(difference.maximumDifference);
+	}
+}
