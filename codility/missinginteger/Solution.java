c155918e398589259a8e54c47042d1af2308b4d7
ChoiMun gyu <cinari4@gmail.com>
2016-09-05 00:07:41 +0900

    apply another solution

--- codility/missinginteger/Solution.java
+++ codility/missinginteger/Solution.java
@@ -1,38 +1,19 @@
-import java.util.Iterator;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.HashSet;
 
 class Solution {
 	public int solution(int[] A) {
-		int result = 0;
-		SortedSet<Integer> resultSet = new TreeSet<Integer>();
+		int min = 1;
+		HashSet<Integer> resultSet = new HashSet<>();
 		for (int a : A) {
-			if (a > 0) {
+			if (a >= 0) {
 				resultSet.add(a);
 			}
 		}
 		
-		Iterator<Integer> it = resultSet.iterator();
-		int min = 0;
-		int missingInt = 0;
-		if (it.hasNext()) {
-			min = it.next();
-			missingInt = min;
+		while (resultSet.contains(min)) {
+			min++;
 		}
 		
-		while (it.hasNext()) {
-			int num = it.next();
-			if (min + 1 != num) {
-				result = min + 1;
-				break;
-			} else {
-				min = num;
-			}
-		}
-		
-		if (result <= 0) {
-			result = missingInt;
-		}
-		return result;
+		return min;
 	}
 }
