542fea3feed12a18424c97e5ddff742fa5a54710
ChoiMun gyu <cinari4@gmail.com>
2016-09-23 23:57:08 +0900

    add not strategy pattern

---
+++ designpattern/strategy/StrategyPatternTest.java
@@ -0,0 +1,12 @@
+package com.github.cinari4;
+
+import org.junit.Test;
+
+public class StrategyPatternTest {
+
+	@Test
+	public void testStrategyPattern() {
+		NotStrategyPattern notStrategy = new NotStrategyPattern();
+	}
+
+}
